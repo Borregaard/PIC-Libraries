8211beafa7a727b02a8381f0873ab4663a6b1a51
Borregaard <frederik.borregaard@gmail.com>
2023-01-08 13:52:18 +0100

    UART for pic16f877a

---
+++ PIC16f877a/UART.X/UART.c
@@ -0,0 +1,13 @@
+/*
+ * File:   UART.c
+ * Author: Frederik
+ *
+ * Created on 8. januar 2023, 12:39
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ PIC16f877a/UART.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Frederik
+ *
+ * Created on 8. januar 2023, 12:21
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
